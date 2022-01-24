27a713b175cb70bbf24b970cb60e43ffc67437cb
Nils Kratochwil <n.kratochwil@posteo.de>
2022-01-24 12:37:24 +0100

    feature create a file

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+import { writeFile } from "fs/promises";
+// import {promises as fs} from "fs"
+
 console.log("hello world");
+
+writeFile("foo", "irgendwas anderes");
+
+writeFile("hello.json", JSON.stringify({ x: 5, y: 6 }, null, 4));
