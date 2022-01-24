9ddfe3fd6f772d51dab71a480bb8e5b03b1c3106
Nils Kratochwil <n.kratochwil@posteo.de>
2022-01-24 16:47:14 +0100

    changes

--- args.js
+++ args.js
@@ -1,9 +1,15 @@
-import { argv } from "process";
-
 // Expected:
 // ["foo", "bar", "baz"]
 
 // command: node args.js foo bar baz
 
-const myArgs = argv.slice(2);
+// option 1
+const myArgs = process.argv.slice(2);
 console.log(myArgs);
+
+// option 2
+const [, , ...args] = process.argv;
+console.log(args);
+
+// option 3
+console.log([process.argv[2], process.argv[3], process.argv[4]]);
