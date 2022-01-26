ade77bde1a61de98dc2deea79cc5300992d20a7f
Nils Kratochwil <n.kratochwil@posteo.de>
2022-01-26 10:23:46 +0100

    feature:generate files for components

---
+++ create-react-component.js
@@ -0,0 +1,64 @@
+import { mkdir, writeFile } from "fs/promises";
+import path from "path";
+import process from "node:process";
+
+const jsxTemplate = () =>
+	`import React from "react";
+
+const ${componentName} = () => {
+    return <div/>
+}
+
+export default ${componentName};
+
+`;
+
+const readmeTemplate = () => `#${componentName}
+
+## How to use this component
+
+...
+`;
+
+// node create-react-component.js
+// npx create-react-app
+
+// API
+// nosw create-react-component.js directory componentName
+// nosw create-react-component.js atoms Button
+// ->
+// * out/atoms/Button/index.jsx
+// * out/atoms/Button/README.md
+
+const outFolder = "out";
+
+// This function has to be called
+const createReactComponent = async (directoryName, componentName) => {
+	const directory = path.join(outFolder, directoryName, componentName);
+	const jsxFileName = path.join(directory, "index.jsx");
+	const readmeFileName = path.join(directory, "README.md");
+	// We can use "await" since this is an async function
+
+	await mkdir(directory, { recursive: true }); // mehrere:, {recursive: true}
+
+	writeFile(jsxFileName, jsxTemplate(componentName))
+		.then(() => {
+			console.log(`${jsxFileName}`);
+		})
+		.catch(error_ => {
+			console.error(error_);
+		});
+
+	writeFile(readmeFileName, readmeTemplate(componentName))
+		.then(() => {
+			console.log(`${readmeFileName}`);
+		})
+		.catch(error_ => {
+			console.error(error_);
+		});
+};
+
+//Read arguments from CLI
+const [directoryName, componentName] = process.argv.slice(2);
+// Add arguments to the create function
+void createReactComponent(directoryName, componentName);
